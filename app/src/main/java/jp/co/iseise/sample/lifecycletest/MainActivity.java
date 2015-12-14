a2f4e70c12771641cc837a81f8417bbef0073625
pri <masuyama@iseise.co.jp>
2015-12-14 11:36:02 +0900

    最初のコミット

---
+++ app/src/main/java/jp/co/iseise/sample/lifecycletest/MainActivity.java
@@ -0,0 +1,57 @@
+package jp.co.iseise.sample.lifecycletest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+	private static final String TAG = "Lifecycle";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button buttonNext = (Button) findViewById(R.id.buttonNext);
+		buttonNext.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, NextActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+	@Override
+	protected void onStart() {
+		Log.d(TAG, "onStart");
+		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		Log.d(TAG, "onResume");
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		Log.d(TAG, "onPause");
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		Log.d(TAG, "onStop");
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		Log.d(TAG, "onDestroy");
+		super.onDestroy();
+	}
+}
